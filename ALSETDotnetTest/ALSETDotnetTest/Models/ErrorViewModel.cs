7a7502e43cd2684463e975f3273081cb63a51a50
PedroLanderos <developerpjlc@gmail.com>
2024-08-15 23:25:13 -0600

    project created

---
+++ ALSETDotnetTest/ALSETDotnetTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ALSETDotnetTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
